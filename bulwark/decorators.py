49284e3a6719dd22cae8bc6b9e016de004e16735
drosengarten <daniel.rosengarten@gmail.com>
2020-01-06 12:08:15 +0000

    can now specify location of dataframe to check
    str:  arg name of the input argument to the decorated function to check
    int:  the entry in a tuple returned by the decorated function to check
    None: check the single dataframe return by the decorated function (default functionality for backward compatibility)

--- bulwark/decorators.py
+++ bulwark/decorators.py
@@ -1,29 +1,102 @@
 """Generates decorators for each check in `checks.py`."""
 import functools
 import sys
-from inspect import getfullargspec, getmembers, isfunction
+from inspect import getfullargspec, getmembers, isfunction, signature, \
+    Parameter
 
 import bulwark.checks as ck
 from bulwark.generic import snake_to_camel
 
 
 class BaseDecorator(object):
+    
+    df_allowed_types = ( int, str, type(None) )
+    
     def __init__(self, *args, **kwargs):
         self.enabled = kwargs.pop("enabled", True)  # setter to enforce bool would be a lot safer
         # self.warn = False ? No - put at func level for all funcs and pass through
+        self.params = getfullargspec(self.check_func).args[1:]
 
-        self.check_func_params = dict(
-            zip(getfullargspec(self.check_func).args[1:], args))
-        self.check_func_params.update(**kwargs)
+        self.__dict__.update(dict(zip(self.params, args)))
+        self.__dict__.update(**kwargs)
+        
+        if type( self._df ) not in self.df_allowed_types:
+            msg = "'df' arg cannot by of type {}.\n" \
+                  "Only allowed types are:\n" \
+                  " str:  arg name of the input argument to the decorated function to check\n" \
+                  " int:  the entry in a tuple returned by the decorated function to check\n" \
+                  " None: check the single dataframe return by the decorated function".format(type(self._df))
+            raise TypeError(msg)
+    
+    @property
+    def _df(self):
+        return self.__dict__.get( 'df', None )
 
+
+
+        
     def __call__(self, f):
+        
+        if type(self._df) is str:
+            if not self._has_arg_name( self._df, f ):
+                raise NameError( "'{}' is not an arg to function '{}'".format(self._df, f.__name__) )
+            
         @functools.wraps(f)
         def decorated(*args, **kwargs):
-            df = f(*args, **kwargs)
-            if self.enabled:
-                self.check_func(df, **self.check_func_params)
-            return df
+            
+            if not self.enabled:
+                return f(*args, **kwargs)
+            
+            check_kwargs = {k: v for k, v in self.__dict__.items()
+                            if k not in ["check_func", "enabled", "params"]}
+            
+            if self._df is None:
+                res = f(*args, **kwargs)
+                check_kwargs['df'] = res
+                self.check_func( **check_kwargs )
+                return res 
+            
+            if type( self._df ) is int: 
+                res = f(*args, **kwargs)
+                if type(res) is not tuple:
+                    raise TypeError('Your function needs to return a tuple to use df=<int> in this decorator')
+                check_kwargs['df'] = res[ self._df ]
+                self.check_func( **check_kwargs )
+                return res
+            
+            if type( self._df ) is str:
+                df_default = self._get_arg_default( f, self._df )
+                df_value = self._get_arg_value( f, self._df, *args, **kwargs )
+                if isinstance(df_default, Parameter.empty) or (df_value is not None):
+                    check_kwargs['df'] = df_value
+                    self.check_func(**check_kwargs)
+                res = f(*args, **kwargs)
+                return res
+
+
+
+
         return decorated
+
+    @staticmethod
+    def _has_arg_name( arg_name, func ):
+        sig = signature( func )
+        res = arg_name in sig.parameters
+        return res
+    
+    @staticmethod
+    def _get_arg_default( func, arg_name ):
+        func_sig = signature( func )
+        res = func_sig.parameters[arg_name].default
+        return res
+    
+    @staticmethod
+    def _get_arg_value( func, arg_name, *args, **kwargs):
+        func_sig = signature( func )
+        bound_args = func_sig.bind(*args, **kwargs)
+        bound_args.apply_defaults()
+        res = bound_args.arguments[ arg_name ]
+        return res
 
 
 def decorator_factory(decorator_name, func):
